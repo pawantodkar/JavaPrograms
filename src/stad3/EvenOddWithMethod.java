ff6d27cb4ec0d31315dfcfe494a10d1348d32c7e
pawantodkar <pwntod98@gmail.com>
2025-11-02 18:05:52 +0530

    The print statement changed

--- src/stad3/EvenOddWithMethod.java
+++ src/stad3/EvenOddWithMethod.java
@@ -6,11 +6,11 @@ public class EvenOddWithMethod
     {
         if (n%2==0)
         {
-            System.out.println("Number is even");
+            System.out.println("00Number is even");
         }
         else
         {
-            System.out.println("Number is odd");
+            System.out.println("01Number is odd");
         }
     }
 
